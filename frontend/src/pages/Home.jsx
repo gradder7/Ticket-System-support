d82e818370630aaa5af5aa3c42c031d392018813
Himanshu <mehra3228@gmail.com>
2023-04-07 16:34:27 +0530

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,23 +1,28 @@
-import { Link } from 'react-router-dom'
-import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 function Home() {
+  const { user } = useSelector((state) => state.auth);
+  const { name } = user;
+  
   return (
     <>
-      <section className='heading'>
+      <section className="heading">
+        <h1>Welcome {name.toUpperCase()}</h1>
         <h1>What do you need help with?</h1>
         <p>Please choose from an option below</p>
       </section>
 
-      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
         <FaQuestionCircle /> Create New Ticket
       </Link>
 
-      <Link to='/tickets' className='btn btn-block'>
+      <Link to="/tickets" className="btn btn-block">
         <FaTicketAlt /> View My Tickets
       </Link>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
