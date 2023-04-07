b3aa0ad0eda2d1681989450b04b3f01da34828f3
Himanshu <mehra3228@gmail.com>
2023-04-07 15:45:24 +0530

    commit

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api/tickets/'
+const API_URL = "https://ticket-system-hkmx.onrender.com/api/tickets/";
 
 // Get ticket notes
 const getNotes = async (ticketId, token) => {
